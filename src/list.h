8b0649c2dcfd005e0410009de0b7c3ac2445341d
GoogleGeek <ffmpeg@gmail.com>
2025-08-24 12:36:51 +0800

    fix compile

---
+++ src/list.h
@@ -0,0 +1,70 @@
+#ifndef __PORTFWD_LIST_H__
+#define __PORTFWD_LIST_H__
+
+#include <stddef.h>
+
+/*
+ * Simple doubly linked list implementation.
+ *
+ * Some of the code is from Linux kernel, and simplified for our purpose.
+ */
+
+struct list_head {
+	struct list_head *next, *prev;
+};
+
+#define LIST_HEAD_INIT(name) { &(name), &(name) }
+
+#define LIST_HEAD(name) \
+	struct list_head name = LIST_HEAD_INIT(name)
+
+static inline void INIT_LIST_HEAD(struct list_head *list)
+{
+	list->next = list;
+	list->prev = list;
+}
+
+static inline void __list_add(struct list_head *new,
+			      struct list_head *prev,
+			      struct list_head *next)
+{
+	next->prev = new;
+	new->next = next;
+	new->prev = prev;
+	prev->next = new;
+}
+
+static inline void list_add(struct list_head *new, struct list_head *head)
+{
+	__list_add(new, head, head->next);
+}
+
+static inline void list_add_tail(struct list_head *new, struct list_head *head)
+{
+	__list_add(new, head->prev, head);
+}
+
+static inline void __list_del(struct list_head * prev, struct list_head * next)
+{
+	next->prev = prev;
+	prev->next = next;
+}
+
+static inline void list_del(struct list_head *entry)
+{
+	__list_del(entry->prev, entry->next);
+	entry->next = NULL;
+	entry->prev = NULL;
+}
+
+#define container_of(ptr, type, member) ({			\
+        const typeof( ((type *)0)->member ) *__mptr = (ptr);	\
+        (type *)( (char *)__mptr - offsetof(type,member) );})
+
+
+#define list_for_each_entry(pos, head, member) \
+	for (pos = container_of((head)->next, typeof(*pos), member); \
+	     &pos->member != (head); \
+	     pos = container_of(pos->member.next, typeof(*pos), member))
+
+#endif /* __PORTFWD_LIST_H__ */
