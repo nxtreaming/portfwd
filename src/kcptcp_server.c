b5b07efd475d99d59aacd5d31de2af9150e5e506
GoogleGeek <ffmpeg@gmail.com>
2025-08-29 13:32:20 +0800

    fix buffer overflow

--- src/kcptcp_server.c
+++ src/kcptcp_server.c
@@ -492,6 +492,17 @@ int main(int argc, char **argv) {
                                                   : INITIAL_BUFFER_SIZE;
                                 if (ncap < c->request.dlen + rem)
                                     ncap = c->request.dlen + rem;
+                                
+                                /* Check buffer size limit before realloc */
+                                if (!buffer_size_check(c->request.capacity,
+                                                       ncap,
+                                                       MAX_TCP_BUFFER_SIZE)) {
+                                    P_LOG_WARN("Buffer size limit exceeded for "
+                                               "connection, closing");
+                                    c->state = S_CLOSING;
+                                    break;
+                                }
+                                
                                 char *np =
                                     (char *)realloc(c->request.data, ncap);
                                 if (!np) {
