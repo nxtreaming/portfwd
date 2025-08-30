70af989cb2af45d4a7414a374cc2836839ba67a6
GoogleGeek <ffmpeg@gmail.com>
2025-08-30 11:38:34 +0800

    fix warning

--- src/common.h
+++ src/common.h
@@ -85,5 +85,6 @@ static inline int ep_add_or_mod(int epfd, int sock, struct epoll_event *ev) {
 #define P_LOG_ERR(fmt, ...) LOG_MSG(LOG_ERR, "ERROR: " fmt, ##__VA_ARGS__)
 #define P_LOG_WARN(fmt, ...) LOG_MSG(LOG_WARNING, "WARN: " fmt, ##__VA_ARGS__)
 #define P_LOG_INFO(fmt, ...) LOG_MSG(LOG_INFO, fmt, ##__VA_ARGS__)
+#define P_LOG_DEBUG(fmt, ...) LOG_MSG(LOG_DEBUG, "DEBUG: " fmt, ##__VA_ARGS__)
 
 #endif /* __PORTFWD_COMMON_H__ */
