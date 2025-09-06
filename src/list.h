a063252a5ceb8cef3836b6dbf116e0531f14eed0
blackswan <nxtreaming@gmail.com>
2025-09-06 16:30:00 +0800

    fix link error

--- src/list.h
+++ src/list.h
@@ -46,7 +46,7 @@ static inline void list_add_tail(struct list_head *new, struct list_head *head) 
  * @head: the head that will follow our entry
  */
 static inline void list_move_tail(struct list_head *list, struct list_head *head) {
-    __list_del_entry(list);
+    __list_del(list->prev, list->next);
     list_add_tail(list, head);
 }
 
